4f0411a13bfa232a2d4d5810196a9a9ca2da06c7
Oubri Daouda <oubridaouda@gmail.com>
2023-04-05 11:41:48 +0000

    update readme

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     <div class="container">
-        <h1 class="project-name">Barka Marchand</h1>
+        <h1 class="project-name">Laravel 10 template</h1>
     </div>
 </x-app-layout>
