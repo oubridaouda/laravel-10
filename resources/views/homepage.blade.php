5c376eee22b3e51ac1958f8d16186a5250c18221
Oubri Daouda <oubridaouda@gmail.com>
2023-04-05 11:40:03 +0000

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="container">
+        <h1 class="project-name">Barka Marchand</h1>
+    </div>
+</x-app-layout>
---
+++ resources/views/vite-asset/vite.blade.php
@@ -0,0 +1,2 @@
+{{--Load vite assets in local--}}
+@vite(['resources/css/app.css','resources/scss/app.scss', 'resources/js/app.js'])
